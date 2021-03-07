56d35de8d00c967558911e4660c3c8e9ad805ff9
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2021-03-07 14:02:34 -0300

    fix: test.sh exporting env vars

--- app/test.sh
+++ app/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export TESTING="True"
+export GITHUB_API_ACCESS_TOKEN="token"
+export GITHUB_API_URL="https://local"
 
 python3 -m pytest --cov=. --cov-branch --cov-report=term --cov-report=html --cov-config=tests/.coveragerc tests "$1"
