b842232a7317ab0ccf01f953c987937026f9d0ee
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2021-03-09 22:42:26 -0300

    feature: creating tests

--- app/tests/routers/test_popular.py
+++ app/tests/routers/test_popular.py
@@ -4,13 +4,13 @@ import pytest
 from exceptions import (
     CalculateScoreException,
     GitHubServiceRequestException,
-    RepositoryForbiddenException,
-    RepositoryMovedPermanently,
     RepositoryNameException,
-    RepositoryNotFoundException,
+    RequestForbiddenException,
+    RequestMovedPermanently,
+    RequestNotFoundException,
 )
 from routers.popular import PopularView
-from schemas.github import GitHupApiResponse
+from schemas.github import GitHupApiOrgResponse, GitHupApiResponse
 from schemas.popular import PopularResponseModel
 from services.cache import CacheService
 from services.github import GitHubService
@@ -69,10 +69,10 @@ def test_check_has_cache(mocker):
 @pytest.mark.parametrize(
     "custom_exception, status_code",
     [
-        (RepositoryNotFoundException, 404),
+        (RequestNotFoundException, 404),
         (RepositoryNameException, 500),
-        (RepositoryForbiddenException, 500),
-        (RepositoryMovedPermanently, 500),
+        (RequestForbiddenException, 500),
+        (RequestMovedPermanently, 500),
         (GitHubServiceRequestException, 500),
     ],
 )
@@ -115,4 +115,111 @@ def test_check_popular_service_exceptions(mocker):
 
     assert response.status_code == 500
     mocked_git_service.get_info.assert_called_once()
+    mocked_popular_service.calculate_score.assert_called_once()
+
+
+def test_check_org_no_cache(mocker):
+    mocked_git_service = mocker.Mock(autospec=GitHubService)
+    mocked_popular_service = mocker.Mock(autospec=PopularService)
+    mocked_cache_service = mocker.Mock(autospec=CacheService)
+
+    view = PopularView(
+        github_service=mocked_git_service, popular_service=mocked_popular_service, cache_service=mocked_cache_service
+    )
+
+    mocked_cache_service.check.return_value = None
+    mocked_git_service.get_org_info.return_value = GitHupApiOrgResponse(
+        items=[GitHupApiResponse(stars=5, name="test", forks=9, owner="lore")]
+    )
+    mocked_popular_service.calculate_score.return_value = PopularResponseModel(
+        score=5, owner="lore", name="test", is_popular=False
+    )
+
+    response = view.check_org(org_name="test")
+
+    assert response.status_code == 200
+    mocked_git_service.get_org_info.assert_called_once()
+    mocked_popular_service.calculate_score.assert_called_once()
+    mocked_cache_service.check.assert_called_once()
+    mocked_cache_service.set_key.assert_called_once()
+
+
+def test_check_org_has_cache(mocker):
+    mocked_git_service = mocker.Mock(autospec=GitHubService)
+    mocked_popular_service = mocker.Mock(autospec=PopularService)
+    mocked_cache_service = mocker.Mock(autospec=CacheService)
+
+    view = PopularView(
+        github_service=mocked_git_service, popular_service=mocked_popular_service, cache_service=mocked_cache_service
+    )
+
+    mocked_cache_service.check.return_value = json.dumps(
+        [{"score": 5, "owner": "lore", "name": "test", "is_popular": False}]
+    )
+    mocked_git_service.get_org_info.return_value = GitHupApiOrgResponse(
+        items=[GitHupApiResponse(stars=5, name="test", forks=9, owner="lore")]
+    )
+    mocked_popular_service.calculate_score.return_value = PopularResponseModel(
+        score=5, owner="lore", name="test", is_popular=False
+    )
+
+    response = view.check_org(org_name="test")
+
+    assert response.status_code == 200
+    mocked_git_service.get_org_info.assert_not_called()
+    mocked_popular_service.calculate_score.assert_not_called()
+    mocked_cache_service.check.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "custom_exception, status_code",
+    [
+        (RequestNotFoundException, 404),
+        (RepositoryNameException, 500),
+        (RequestForbiddenException, 500),
+        (RequestMovedPermanently, 500),
+        (GitHubServiceRequestException, 500),
+    ],
+)
+def test_check_org_github_service_exceptions(mocker, custom_exception, status_code):
+    mocked_git_service = mocker.Mock(autospec=GitHubService)
+    mocked_popular_service = mocker.Mock(autospec=PopularService)
+    mocked_cache_service = mocker.Mock(autospec=CacheService)
+
+    view = PopularView(
+        github_service=mocked_git_service, popular_service=mocked_popular_service, cache_service=mocked_cache_service
+    )
+
+    mocked_cache_service.check.return_value = None
+    mocked_git_service.get_org_info.side_effect = custom_exception
+    mocked_popular_service.calculate_score.return_value = PopularResponseModel(
+        score=5, owner="lore", name="test", is_popular=False
+    )
+
+    response = view.check_org(org_name="test")
+
+    assert response.status_code == status_code
+    mocked_git_service.get_org_info.assert_called_once()
+    mocked_popular_service.calculate_score.assert_not_called()
+
+
+def test_check_org_popular_service_exceptions(mocker):
+    mocked_git_service = mocker.Mock(autospec=GitHubService)
+    mocked_popular_service = mocker.Mock(autospec=PopularService)
+    mocked_cache_service = mocker.Mock(autospec=CacheService)
+
+    view = PopularView(
+        github_service=mocked_git_service, popular_service=mocked_popular_service, cache_service=mocked_cache_service
+    )
+
+    mocked_cache_service.check.return_value = None
+    mocked_git_service.get_org_info.return_value = GitHupApiOrgResponse(
+        items=[GitHupApiResponse(stars=5, name="test", forks=9, owner="lore")]
+    )
+    mocked_popular_service.calculate_score.side_effect = CalculateScoreException
+
+    response = view.check_org(org_name="test")
+
+    assert response.status_code == 500
+    mocked_git_service.get_org_info.assert_called_once()
     mocked_popular_service.calculate_score.assert_called_once()
--- app/tests/services/test_github.py
+++ app/tests/services/test_github.py
@@ -2,12 +2,12 @@ import pytest
 from config import GITHUB_API_ACCESS_TOKEN, GITHUB_API_URL
 from exceptions import (
     GitHubServiceRequestException,
-    RepositoryForbiddenException,
-    RepositoryMovedPermanently,
     RepositoryNameException,
-    RepositoryNotFoundException,
+    RequestForbiddenException,
+    RequestMovedPermanently,
+    RequestNotFoundException,
 )
-from schemas.github import GitHupApiResponse
+from schemas.github import GitHupApiOrgResponse, GitHupApiResponse
 from services.github import GitHubService
 
 
@@ -30,9 +30,9 @@ def test_get_info_repository_name_invalid(requests_mock):
 @pytest.mark.parametrize(
     "status_code, custom_exception",
     [
-        (403, RepositoryForbiddenException),
-        (404, RepositoryNotFoundException),
-        (301, RepositoryMovedPermanently),
+        (403, RequestForbiddenException),
+        (404, RequestNotFoundException),
+        (301, RequestMovedPermanently),
         (500, GitHubServiceRequestException),
     ],
 )
@@ -51,3 +51,39 @@ def test_get_info_github_request_fail_invalid(requests_mock):
 
     with pytest.raises(GitHubServiceRequestException):
         service.get_info(repository_name="test/test")
+
+
+def test_get_org_info(requests_mock):
+    requests_mock.get(
+        f"{GITHUB_API_URL}/orgs/test/repos", json=[{"stargazers_count": 0, "forks_count": 0, "name": "test"}]
+    )
+    service = GitHubService(url=GITHUB_API_URL, access_token=GITHUB_API_ACCESS_TOKEN)
+    result = service.get_org_info(org_name="test")
+
+    assert result == GitHupApiOrgResponse(items=[GitHupApiResponse(stars=0, forks=0, owner="test", name="test")])
+
+
+@pytest.mark.parametrize(
+    "status_code, custom_exception",
+    [
+        (403, RequestForbiddenException),
+        (404, RequestNotFoundException),
+        (301, RequestMovedPermanently),
+        (500, GitHubServiceRequestException),
+    ],
+)
+def test_get_org_info_github_request_fail(requests_mock, status_code, custom_exception):
+
+    requests_mock.get(f"{GITHUB_API_URL}/orgs/test/repos", status_code=status_code)
+    service = GitHubService(url=GITHUB_API_URL, access_token=GITHUB_API_ACCESS_TOKEN)
+
+    with pytest.raises(custom_exception):
+        service.get_org_info(org_name="test")
+
+
+def test_get_org_info_github_request_fail_invalid(requests_mock):
+    requests_mock.get(f"{GITHUB_API_URL}/orgs/test/repos", exc=Exception)
+    service = GitHubService(url=GITHUB_API_URL, access_token=GITHUB_API_ACCESS_TOKEN)
+
+    with pytest.raises(GitHubServiceRequestException):
+        service.get_org_info(org_name="test")
